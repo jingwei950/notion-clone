3fbd6380e83d495965f145020e1d0f4b3c2cba18
Jing Wei <jingwei950@hotmail.sg>
2024-07-21 16:26:18 +0800

    feat(Document): Build Document page

---
+++ src/components/Document.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { FormEvent, useEffect, useState, useTransition } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+
+function Document({ id }: { id: string }) {
+  const [data, loading, error] = useDocumentData(doc(db, "documents", id));
+  const [input, setInput] = useState("");
+  const [isUpdating, startTransition] = useTransition();
+
+  useEffect(() => {
+    if (!data) return;
+    setInput(data.title);
+  }, [data]);
+
+  const updateTitle = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (input.trim()) {
+      startTransition(async () => {
+        await updateDoc(doc(db, "documents", id), {
+          title: input,
+        });
+      });
+    }
+  };
+
+  return (
+    <div>
+      {/* Title */}
+      <div className="flex max-w-6xl mx-auto justify-between pb-5">
+        <form
+          className="flex flex-1 space-x-2"
+          onSubmit={updateTitle}
+        >
+          {/* Upadte title... */}
+          <Input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+
+          <Button type="submit">{isUpdating ? "Updating..." : "Update"}</Button>
+
+          {/* If */}
+          {/* isOwner && InviteUser, DeleteDocument */}
+        </form>
+      </div>
+
+      <div>
+        {/* Manage Users */}
+
+        {/* Avatars */}
+      </div>
+
+      {/* Collaborative Editor */}
+    </div>
+  );
+}
+export default Document;
